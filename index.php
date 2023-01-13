bb9310c952ebafba71b24b4023b70c1b2f08dd9c
dimmascool <dimasadam058@gmail.com>
2023-01-14 00:55:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-    echo "hello world";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+</head>
+<body>
+    <a href="https://www.postman.com/orbital-module-candidate-45560323/workspace/ppma2-kel-macan/request/20159862-94a6028f-a8f0-477e-9e66-84830179cdba">Klik disini untuk melihat petunjuk penggunaan API Kelompok macan PPMA2 </a>
+</body>
+</html>
