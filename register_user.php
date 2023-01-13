698753f9153fe85145224034f1319d034a8fe8fd
dimmascool <dimasadam058@gmail.com>
2023-01-14 00:07:32 +0700

    Update register_user.php

--- register_user.php
+++ register_user.php
@@ -1,22 +1,30 @@
 <?php
+    header("Content-type: Application/json");
+    
     require 'koneksi.php';
 
     if($_SERVER['REQUEST_METHOD']=='POST'){
 
         $username = $_POST['username'];
         $password = $_POST['password'];
-
         $dateNow = date("Y-m-d h:i:s");
 
-        $query = $koneksi->query("INSERT INTO `user` (`username`, `password`, `create_on`) VALUES ('$username','$password','$dateNow')");
+        // checking username if already exist
 
-        if ($query) {
-            http_response_code(200);
-            header("Content-type: Application/json");
-            echo json_encode(array('message' => 'success'));
+        $query_checking = $koneksi->query("SELECT * FROM user WHERE username = '$username'");
+
+        if($query_checking->num_rows > 0) {
+            http_response_code(409);
+            echo json_encode(array('message' => 'username already exist'));
         } else {
-            http_response_code(500);
-            echo json_encode(array('message' => $koneksi->error));
+            $query = $koneksi->query("INSERT INTO `user` (`username`, `password`, `create_on`) VALUES ('$username','$password','$dateNow')");
+            if ($query) {
+                http_response_code(200);
+                echo json_encode(array('message' => 'success'));
+            } else {
+                http_response_code(500);
+                echo json_encode(array('message' => $koneksi->error));
+            } 
         }
     }    
 ?>
